a2e8b4c7f1ff9a9d9857356fb2ab5ec38a612739
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2020-10-09 11:22:00 +0200

    Support shorthands

--- src/index.js
+++ src/index.js
@@ -1,11 +1,81 @@
 import { pathToRegexp } from "./path-to-regex-1.7.esm.js";
 
+export function parseShorthand(str) {
+  let protocol = "";
+  let hostname = "";
+  let pathname = "";
+  let search = "";
+  let hash = "";
+
+  let i = str.indexOf("://");
+  if (i !== -1) {
+    protocol = str.substring(0, i);
+    str = str.substring(i + 3);
+
+    i = str.indexOf("/")
+    hostname = str.substring(0, i);
+    str = str.substring(i + 1);
+  }
+
+  i = str.indexOf("#");
+  if (i !== -1) {
+    hash = str.substring(i + 1);
+    str = str.substring(0, i);
+  }
+
+  str = str.replace(/(:\w+)\?/g, (_, name) => name + "§").replace(/\*\?/g,"*§").replace(/\)\?/g, ")§");
+  i = str.indexOf("?")
+
+  if (i !== -1) {
+    pathname = str.substring(0, i).replace("§", "?");
+    search = str.substring(i + 1).replace("§", "?");
+  } else {
+    pathname = str.replace("§", "?");
+  }
+
+  return { protocol, hostname, pathname, search, hash }
+}
+
 export class URLPattern {
   #pattern = {};
   #regexp = {};
   #keys = {};
 
-  constructor({ baseURL, pathname, protocol, username, password, hostname, port, search, hash }) {
+  constructor(...args) {
+    let baseURL = null;
+    let options = {};
+
+    if (typeof args[0] === "object") {
+      options = args[0];
+      baseURL = options.baseURL;
+    } 
+    // shorthand
+    else if (typeof args[0] === string) {
+      options = parseShorthand(args[0]);
+      if (args[1]) {
+        if (typeof args[1] === "string") {
+          baseURL = args[1];
+        } else {
+          throw TypeError;
+        }
+      }
+    } 
+    // invalid arguments
+    else {
+      throw TypeError;
+    }
+    
+    const {
+      protocol,
+      username,
+      password,
+      hostname,
+      port,
+      pathname,
+      search,
+      hash
+    } = options;
+
     // If none of the URL part patterns are specified then the constructor will throw an exception. 
     if (!protocol && !username && !password && !hostname && !port && !pathname && !search && !hash) {
       throw TypeError;
@@ -43,14 +113,14 @@ export class URLPattern {
     }
 
     // Override with arguments given to ctor.
-    if (pathname) this.#pattern.pathname = pathname;
-    if (protocol) this.#pattern.protocol = protocol;
-    if (username) this.#pattern.username = username;
-    if (password) this.#pattern.password = password;
-    if (hostname) this.#pattern.hostname = hostname;
-    if (port) this.#pattern.port = port;
-    if (search) this.#pattern.search = search;
-    if (hash) this.#pattern.hash = hash;
+    if (pathname) this.#pattern.pathname = options.pathname;
+    if (protocol) this.#pattern.protocol = options.protocol;
+    if (username) this.#pattern.username = options.username;
+    if (password) this.#pattern.password = options.password;
+    if (hostname) this.#pattern.hostname = options.hostname;
+    if (port) this.#pattern.port = options.port;
+    if (search) this.#pattern.search = options.search;
+    if (hash) this.#pattern.hash = options.hash;
 
     // If the baseURL is missing and the pathname is relative then an exception is thrown
     let isRelativePath = !this.#pattern.pathname.startsWith("/");
@@ -145,7 +215,15 @@ export class URLPatternList {
     } else {
       try {
         for (let patternInit of list) {
-          const init = Object.assign(Object.assign({}, options), patternInit);
+          let init = {};
+          if (typeof patternInit === "object") {
+            init = Object.assign(Object.assign({}, options), patternInit);
+          } else if (typeof patternInit === "string") {
+            init = Object.assign(Object.assign({}, options), parseShorthand(patternInit));
+          } else {
+            throw TypeError;
+          }
+
           this.#patterns.push(new URLPattern(init));
         }
       } catch {
