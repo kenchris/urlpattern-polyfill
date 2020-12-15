2e9dbaa4ef20d4afb38efee9a2e56799a34b56ff
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2020-12-15 17:35:46 +0100

    Add more tests incl for shorthands

--- test.js
+++ test.js
@@ -1,7 +1,55 @@
 import test from 'ava';
-import {URLPattern, URLPatternList} from './dist/index.js';
+import {URLPattern, URLPatternList, parseShorthand} from './dist/index.js';
 
 const baseURL = 'https://example.com';
+
+test("shorthands 1", t => {
+  let {
+    protocol, hostname, pathname, search, hash
+  } = parseShorthand("https://google.com/*:hest?/?q=s#hash");
+
+  t.true(protocol === "https");
+  t.true(hostname === "google.com");
+  t.true(pathname === '*:hest?/');
+  t.true(search === 'q=s');
+  t.true(hash === 'hash');
+});
+
+test("shorthands 2", t => {
+  let {
+    protocol, hostname, pathname, search, hash
+  } = parseShorthand("https://google.com/foo/*?/?q=s#hash");
+
+  t.true(protocol === "https");
+  t.true(hostname === "google.com");
+  t.true(pathname === 'foo/*?/');
+  t.true(search === 'q=s');
+  t.true(hash === 'hash');
+});
+
+test("shorthands 3", t => {
+  let {
+    protocol, hostname, pathname, search, hash
+  } = parseShorthand("https://google.com/bar/(.*)?/?q=s#hash");
+
+  t.true(protocol === "https");
+  t.true(hostname === "google.com");
+  t.true(pathname === 'bar/(.*)?/');
+  t.true(search === 'q=s');
+  t.true(hash === 'hash');
+});
+
+test("shorthands 4", t => {
+  let {
+    protocol, hostname, pathname, search, hash
+  } = parseShorthand("/product/:type*");
+
+  t.true(protocol === '');
+  t.true(hostname === '');
+  t.true(pathname === '/product/:type*');
+  t.true(search === '');
+  t.true(hash === '');
+});
 
 test('urlPattern', t => {
   let pattern = new URLPattern({ baseURL, pathname: '/product/*?' });
@@ -20,5 +68,16 @@ test('JavaScript URL routing 1/2', t => {
   t.false(result);
 
   result = imagePattern.exec("https://example.com/images/thumbs/flower.png");
-  t.true(result.pathname.groups['imagetype'] === "png"); 
+  t.true(result.pathname.groups['imagetype'] === "png");
+});
+
+test('JavaScript URL routing 2/2', t => {
+  const apiPattern = new URLPattern({
+    pathname: '/api/:product/:param?'
+  });
+
+  let result = apiPattern.exec(baseURL + '/api/videos/12');
+
+  t.true(result.pathname.groups['product'] === "videos");
+  t.true(result.pathname.groups['param'] === "12");
 });
