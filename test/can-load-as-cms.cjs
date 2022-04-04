50e6b8787ba5a8b10e4d857f2662eaaedb7651f4
sanderelias <sanderelias+git@gmail.com>
2022-04-04 10:23:44 +0200

    remove newline anf package.json

--- test/can-load-as-cms.cjs
+++ test/can-load-as-cms.cjs
@@ -4,7 +4,6 @@ const test  = require( "ava");
 
 const baseURL = 'https://example.com';
 
-
 test('urlPattern', t => {
   let pattern = new URLPattern({baseURL, pathname: '/product/*?'});
   t.true(pattern.test(baseURL + '/product/a/b'));
