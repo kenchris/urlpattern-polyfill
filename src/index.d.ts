f86ddcbfc824ee345dd66ad3551cd1c9905e3679
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-05-09 16:58:14 +0200

    fix: declare class types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,53 +1,51 @@
 export {};
 
-declare global {
- type URLPatternInput = URLPatternInit | string;
+type URLPatternInput = URLPatternInit | string;
 
-  class URLPattern {
-    constructor(init?: URLPatternInput, baseURL?: string);
+export declare class URLPattern {
+  constructor(init?: URLPatternInput, baseURL?: string);
 
-    test(input?: URLPatternInput, baseURL?: string): boolean;
+  test(input?: URLPatternInput, baseURL?: string): boolean;
 
-    exec(input?: URLPatternInput, baseURL?: string): URLPatternResult | null;
+  exec(input?: URLPatternInput, baseURL?: string): URLPatternResult | null;
 
-    readonly protocol: string;
-    readonly username: string;
-    readonly password: string;
-    readonly hostname: string;
-    readonly port: string;
-    readonly pathname: string;
-    readonly search: string;
-    readonly hash: string;
-  }
+  readonly protocol: string;
+  readonly username: string;
+  readonly password: string;
+  readonly hostname: string;
+  readonly port: string;
+  readonly pathname: string;
+  readonly search: string;
+  readonly hash: string;
+}
 
-  interface URLPatternInit {
-    baseURL?: string;
-    username?: string;
-    password?: string;
-    protocol?: string;
-    hostname?: string;
-    port?: string;
-    pathname?: string;
-    search?: string;
-    hash?: string;
-  }
+interface URLPatternInit {
+  baseURL?: string;
+  username?: string;
+  password?: string;
+  protocol?: string;
+  hostname?: string;
+  port?: string;
+  pathname?: string;
+  search?: string;
+  hash?: string;
+}
 
-  interface URLPatternResult {
-    inputs: [URLPatternInput];
-    protocol: URLPatternComponentResult;
-    username: URLPatternComponentResult;
-    password: URLPatternComponentResult;
-    hostname: URLPatternComponentResult;
-    port: URLPatternComponentResult;
-    pathname: URLPatternComponentResult;
-    search: URLPatternComponentResult;
-    hash: URLPatternComponentResult;
-  }
+interface URLPatternResult {
+  inputs: [URLPatternInput];
+  protocol: URLPatternComponentResult;
+  username: URLPatternComponentResult;
+  password: URLPatternComponentResult;
+  hostname: URLPatternComponentResult;
+  port: URLPatternComponentResult;
+  pathname: URLPatternComponentResult;
+  search: URLPatternComponentResult;
+  hash: URLPatternComponentResult;
+}
 
-  interface URLPatternComponentResult {
-    input: string;
-    groups: {
-        [key: string]: string | undefined;
-    };
-  }
+interface URLPatternComponentResult {
+  input: string;
+  groups: {
+      [key: string]: string | undefined;
+  };
 }
